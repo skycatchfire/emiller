3f6b8e8c484a97225ce8d9fd0914714daa70cdd7
TR2E <theroad2eldorado@gmail.com>
2019-09-26 12:12:45 -0400

    Add files via upload

---
+++ js/emiller.js
@@ -0,0 +1,233 @@
+(function($) {
+  $(function() {
+    var signupChannel = localStorage.getItem('emiller.signup-channel');
+    if (signupChannel && $('#mce-CHANNEL').length) {
+      $('#mce-CHANNEL').val(signupChannel);
+    }
+
+    // Don't show promotion if completed.
+    var hasCompletionCookie = $.cookie('emiller.popup_promotion.completed');
+    if (hasCompletionCookie) return;
+
+    var $component = $('[data-subscriber-overlay]');
+
+    // Check that the data-video-hero component exists
+    if ($component.length <= 0) {
+      return;
+    }
+
+    var $close = $component.find('[data-subscriber-overlay-close]');
+    var $remindMe = $component.find('[data-subscriber-overlay-remind-me]');
+    var $complete = $component.find('[data-subscriber-overlay-complete]');
+
+    var hasPromotionCookie = $.cookie('emiller.popup_promotion.remind');
+
+    function initClickHandlers() {
+      $close.on('click', function() {
+        closeOverlay();
+
+        if (typeof dataLayer !== 'undefined') {
+          dataLayer.push({
+            event: 'overlay_closed',
+          });
+        }
+      });
+
+      $remindMe.on('click', function() {
+        closeOverlay();
+
+        if (typeof dataLayer !== 'undefined') {
+          dataLayer.push({
+            event: 'overlay_remind_me',
+          });
+        }
+      });
+
+      $complete.on('click', function() {
+        if (typeof dataLayer !== 'undefined') {
+          dataLayer.push({
+            event: 'overlay_completed',
+          });
+        }
+
+        $.cookie('emiller.popup_promotion.completed', 1, { expires: 10 * 365 });
+        localStorage.setItem('emiller.signup-channel', 'Subscriber Overlay');
+      });
+    }
+
+    function initEventHandlers() {
+      $component.bind('keyup', function(event) {
+        // Esc
+        if (event.keyCode === 27) {
+          closeOverlay();
+        }
+      });
+
+      $('[data-tabloop-subscribe]').tabloop();
+    }
+
+    function closeOverlay() {
+      $.cookie('emiller.popup_promotion.remind', 1, { expires: 30 });
+      $component.removeClass('visible');
+
+      // Reset focus to page top
+      $('[data-tabloop-subscribe]').unbind();
+      $('[data-tabloop-subscribe]').attr('tabindex', '-1');
+      $('#skip-link').focus();
+    }
+
+    function showPromotion(exists) {
+      if (exists) return;
+
+      setTimeout(function() {
+        $component.addClass('visible');
+        $close.focus();
+        $('[data-tabloop-subscribe]').attr('tabindex', '1');
+
+        if (typeof dataLayer !== 'undefined') {
+          dataLayer.push({
+            event: 'overlay_seen',
+          });
+        }
+      }, 5000);
+    }
+
+    // ESC to close modal
+    // Tab locks within modal
+
+    // INIT
+    showPromotion(hasPromotionCookie);
+    initClickHandlers();
+    initEventHandlers();
+  });
+
+  $.fn.tabloop = function() {
+    var p = {
+      collection: this,
+    };
+    $(this).keydown(p, function(e) {
+      if ($(e.currentTarget)[0] == e.data.collection.last()[0]) {
+        e.data.collection.first().focus();
+        e.preventDefault();
+      }
+    });
+  };
+})(jQuery, this);
+
+/*!
+ * jQuery Cookie Plugin v1.4.1
+ * https://github.com/carhartl/jquery-cookie
+ *
+ * Copyright 2006, 2014 Klaus Hartl
+ * Released under the MIT license
+ */
+(function(factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD (Register as an anonymous module)
+    define(['jquery'], factory);
+  } else if (typeof exports === 'object') {
+    // Node/CommonJS
+    module.exports = factory(require('jquery'));
+  } else {
+    // Browser globals
+    factory(jQuery);
+  }
+})(function($) {
+  var pluses = /\+/g;
+
+  function encode(s) {
+    return config.raw ? s : encodeURIComponent(s);
+  }
+
+  function decode(s) {
+    return config.raw ? s : decodeURIComponent(s);
+  }
+
+  function stringifyCookieValue(value) {
+    return encode(config.json ? JSON.stringify(value) : String(value));
+  }
+
+  function parseCookieValue(s) {
+    if (s.indexOf('"') === 0) {
+      // This is a quoted cookie as according to RFC2068, unescape...
+      s = s
+        .slice(1, -1)
+        .replace(/\\"/g, '"')
+        .replace(/\\\\/g, '\\');
+    }
+
+    try {
+      // Replace server-side written pluses with spaces.
+      // If we can't decode the cookie, ignore it, it's unusable.
+      // If we can't parse the cookie, ignore it, it's unusable.
+      s = decodeURIComponent(s.replace(pluses, ' '));
+      return config.json ? JSON.parse(s) : s;
+    } catch (e) {}
+  }
+
+  function read(s, converter) {
+    var value = config.raw ? s : parseCookieValue(s);
+    return $.isFunction(converter) ? converter(value) : value;
+  }
+
+  var config = ($.cookie = function(key, value, options) {
+    // Write
+
+    if (arguments.length > 1 && !$.isFunction(value)) {
+      options = $.extend({}, config.defaults, options);
+
+      if (typeof options.expires === 'number') {
+        var days = options.expires,
+          t = (options.expires = new Date());
+        t.setMilliseconds(t.getMilliseconds() + days * 864e5);
+      }
+
+      return (document.cookie = [
+        encode(key),
+        '=',
+        stringifyCookieValue(value),
+        options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+        options.path ? '; path=' + options.path : '',
+        options.domain ? '; domain=' + options.domain : '',
+        options.secure ? '; secure' : '',
+      ].join(''));
+    }
+
+    // Read
+
+    var result = key ? undefined : {},
+      // To prevent the for loop in the first place assign an empty array
+      // in case there are no cookies at all. Also prevents odd result when
+      // calling $.cookie().
+      cookies = document.cookie ? document.cookie.split('; ') : [],
+      i = 0,
+      l = cookies.length;
+
+    for (; i < l; i++) {
+      var parts = cookies[i].split('='),
+        name = decode(parts.shift()),
+        cookie = parts.join('=');
+
+      if (key === name) {
+        // If second argument (value) is a function it's a converter...
+        result = read(cookie, value);
+        break;
+      }
+
+      // Prevent storing a cookie that we couldn't decode.
+      if (!key && (cookie = read(cookie)) !== undefined) {
+        result[name] = cookie;
+      }
+    }
+
+    return result;
+  });
+
+  config.defaults = {};
+
+  $.removeCookie = function(key, options) {
+    // Must not alter options, thus extending a fresh object...
+    $.cookie(key, '', $.extend({}, options, { expires: -1 }));
+    return !$.cookie(key);
+  };
+});
